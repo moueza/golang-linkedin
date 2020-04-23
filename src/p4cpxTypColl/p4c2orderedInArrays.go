8e2eedb296ac3b350f6df66a4348feb2a091b9dd
moueza <>
2020-04-23 04:19:24 +0200

    rename

---
+++ src/p4cpxTypColl/p4c2orderedInArrays.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+var  colors [3]string
+colors[0]="Red"
+colors[1]="Green"
+colors[2]="Blue"
+fmt.Println(colors)
+fmt.Println(colors[1])
+
+
+var numbers = [5]int{5,3,1,2,4}
+fmt.Println(numbers)
+
+fmt.Println(len(numbers))
+
+}
